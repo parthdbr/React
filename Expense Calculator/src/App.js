f7a36b8b8db0591d79ff3ae2015f12b9c382b231
Parth Dobariya <parthdbr@gmail.com>
2023-07-16 15:15:03 +0530

    Initial Commit

---
+++ Expense Calculator/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+      <>
+     
+         <div>
+          Hello
+         </div>
+      
+      </>
+  );
+}
+
+export default App;
