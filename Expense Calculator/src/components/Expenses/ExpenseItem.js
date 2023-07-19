08151ebb7789b494a4afffe31e3a506659388cef
Parth Dobariya <parthdbr@gmail.com>
2023-07-19 16:58:16 +0530

    Rendering Lists

--- Expense Calculator/src/components/Expenses/ExpenseItem.js
+++ Expense Calculator/src/components/Expenses/ExpenseItem.js
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
 
 function ExpenseItem(props) {
 
-    const [title, setTitle] = useState(props.title)
+    // const [title, setTitle] = useState(props.title)
 
     // const clickHandler = () => {
     //    setTitle('Updatedd!!');
@@ -17,7 +17,7 @@ function ExpenseItem(props) {
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             {/* <button onClick={clickHandler}>Change Title</button> */}
--- Expense Calculator/src/components/Expenses/Expenses.js
+++ Expense Calculator/src/components/Expenses/Expenses.js
@@ -27,17 +27,29 @@ function Expenses(props) {
     
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {/* <p>Data for years {filterInfoText} is hidden.</p> */}
-      {props.items.map((expense) => (
+      {/* {props.items.map((expense) => (
       <ExpenseItem 
+        key={expense.id}
         title={expense.title} 
         amount={expense.amount} 
         date={expense.date} />
-      ))}
-      
+      ))} */}
+      {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   );
 }
