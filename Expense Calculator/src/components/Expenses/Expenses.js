b66062e2f2840eba64dc3659e7b336acf0be9d6b
Parth Dobariya <parthdbr@gmail.com>
2023-07-19 17:19:09 +0530

    Show by selection

--- Expense Calculator/src/components/Expenses/Expenses.js
+++ Expense Calculator/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
-import ExpenseItem from './ExpenseItem';
+
 import './Expenses.css';
 import Card from '../UI/Card';
 import React, { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
-
+import ExpensesList from './ExpensesList';
 
 
 function Expenses(props) {
@@ -19,24 +19,12 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if(filteredExpenses.length > 0) {
-    expensesContent =  filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
 
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     
     </Card>
   );
