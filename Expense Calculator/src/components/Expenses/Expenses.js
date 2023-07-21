c855d4262384dc90c3b20c349351d2521036c7ec
Parth Dobariya <parthdbr@gmail.com>
2023-07-21 20:04:22 +0530

    Show by selection

--- Expense Calculator/src/components/Expenses/Expenses.js
+++ Expense Calculator/src/components/Expenses/Expenses.js
@@ -5,7 +5,6 @@ import React, { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 
-
 function Expenses(props) {
 
   const [filteredYear, setFilteredYear] = useState('2023');
@@ -18,7 +17,6 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-
 
   return (
     <Card className="expenses">
