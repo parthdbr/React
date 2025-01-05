5fc0cafc662705619a581d582133f59a1eb731d9
Parth <parthdbr@gmail.com>
2025-01-05 17:05:22 +0530

    NavBar

--- word-count/src/componants/Navbar.js
+++ word-count/src/componants/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Woed</a>
+            <a className="navbar-brand" href="#">Word Count</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
